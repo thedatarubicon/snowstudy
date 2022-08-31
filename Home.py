61695f56b45a0f513f6040416e1fd885bb9ed896
Emily Nguyen <101374243+thedatarubicon@users.noreply.github.com>
2022-08-31 12:27:25 -0500

    Update Homepage
    Update Homepage with the following sections: table of contents, page overview, and references.

--- Home.py
+++ Home.py
@@ -3,3 +3,18 @@ import streamlit as st
 
 # App Code
 st.header("SnowPro Study Guide Randomizer")
+
+st.subheader("Table of Contents")
+st.markdown("I. SnowPro Core")
+st.markdown("II. SnowPro Advanced: Architect")
+st.markdown("III. SnowPro Advanced: Administrator")
+st.markdown("IV. SnowPro Advanced: Data Engineer")
+st.markdown("V. SnowPro Advanced: Data Scientist")
+st.markdown("")
+st.subheader("Page Overview")
+st.markdown("A. Exam Breakdown")
+st.markdown("B. Randomizer")
+st.markdown("C. Sample Questions")
+st.markdown("")
+st.subheader("References")
+st.markdown("[Snowflake Certifications](https://www.snowflake.com/certifications/)")
