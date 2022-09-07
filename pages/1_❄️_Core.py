a3daf0f8fbb077d5b7df0abd52ff288e65255ae3
Emily Nguyen <101374243+thedatarubicon@users.noreply.github.com>
2022-09-07 17:47:59 -0500

    Restructure and update pages
    Restructured and updated exam-specific pages by adding in new tabs, adding content to the domains and objectives tab, modifying the page layout, and injecting CSS to help with markdown formatting.

--- pages/1_❄️_Core.py
+++ pages/1_❄️_Core.py
@@ -8,19 +8,189 @@ import pandas as pd
 import random
 from links.core import links
 
+# Page Layout
+st.set_page_config(layout="wide")
+
 # App code
 st.header("SnowPro Core")
 
-tab1, tab2, tab3 = st.tabs(["Exam Breakdown", "Randomizer", "Sample Questions"])
+# Tabs
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Exam Breakdown", "Domains & Objectives", "Selector", "Randomizer", "Sample Questions"])
 
+# Exam Breakdown
 with tab1:
     st.subheader("Exam Breakdown")
-    ebd = {"Domain": ["Account & Security", "Virtal Warehouses", "Data Movement", "Performance Management", "Snowflake Overview and Architecture", "Storage and Protection"],
+    ebd = {"Domain": ["Account & Security", "Virtual Warehouses", "Data Movement", "Performance Management", "Snowflake Overview and Architecture", "Storage and Protection"],
            "Estimated Percentage Range": ["10-15%", "15-20%", "10-20%", "5-10%", "25-30%", "10-15%"]}
     df = pd.DataFrame(data=ebd)
     st.table(df)
 
+# Domains & Objectives
 with tab2:
+    st.subheader("Domains & Objectives")
+    with st.container():
+        tab6, tab7, tab8, tab9, tab10, tab11 = st.tabs(["Account & Security", "Virtual Warehouses", "Data Movement", "Performance Management", "Snowflake Overview and Architecture", "Storage and Protection"])
+        st.markdown('''
+                    <style>
+                    [data-testid="stMarkdownContainer"] ul{
+                        padding-left:40px;
+                    }
+                    </style>
+                    ''', unsafe_allow_html=True)
+        # Account & Security
+        with tab6:
+            st.markdown("""
+            ##### 1.1 Explain how to manage Snowflake accounts.
+            - Account usage
+            - Account access
+            - Account views
+            - Information schema
+            ##### 1.2 Outline security principles.
+            - Multi-factor Authentication (MFA)
+            - Data Encryption
+            - Network Security and Policies
+            - Access Control
+            - Federated Authentication
+            - Single Sign-On (SSO)
+            ##### 1.3 Define the entities and roles that are used in Snowflake.
+            - Outline how privileges can be granted and revoked
+            - Explain Role Hierarchy and Privilege Inheritance
+            ##### 1.4 Explain the security capabilities associated with each Snowflake edition.
+            - Column-level security
+            ##### 1.5 Outline data governance capabilities in Snowflake.
+            - Data masking
+            - Account usage views
+            - External tokenization
+            - Secure views
+            """)
+        
+        # Virtual Warehouses
+        with tab7:
+            st.markdown("""
+            ##### 2.1 Outline compute principles.
+            - Credit usage and billing
+            - Concurrency
+            - Caching
+            - Virtual warehouse characteristics and parameters
+            - Query profiler
+            ##### 2.2 Explain Virtual Warehouse best practices.
+            - Scale up compared to scale out
+            - Types of virtual warehouses
+            - Management/monitoring
+            """)
+      
+        # Data Movement
+        with tab8:
+            st.markdown("""
+            ##### 3.1 Outline different commands used to load data and when they should be used.
+            - COPY
+            - INSERT
+            - PUT
+            - GET
+            - VALIDATE
+            - SNOWPIPE
+            - COPY INTO
+            ##### 3.2 Define bulk as compared to continuous data loading methods.
+            - COPY
+            - Snowpipe
+            - Parameters types
+            ##### 3.3 Define best practices that should be considered when loading data.
+            - File size
+            - File Formats
+            - Folders
+            ##### 3.4 Outline how data can be unloaded from Snowflake to either local storage or cloud storage locations.
+            - Define formats supported for unloading data from Snowflake
+            - Define commands that help when unloading data
+            - Define best practices that should be considered when unloading data
+            ##### 3.5 Explain how to work and load semi-structured data.
+            - Supported file formats, data types, and sizes
+            - VARIANT column
+            - Flattening the nested structure
+            """)
+        
+        # Performance Management
+        with tab9:
+            st.markdown("""
+            ##### 4.1 Outline best practices for Snowflake performance management on storage.
+            - Clustering
+            - Materialized views
+            - Search Optimization Service
+            ##### 4.2 Outline best practices for Snowflake performance management on virtual warehouses.
+            - Query performance and analysis
+            - Query profiles
+            - Query history and activity
+            - SQL optimization
+            - Caching
+            """)
+
+        # Snowflake Overview and Architecture
+        with tab10:
+            st.markdown("""
+            ##### 5.1 Outline key components of Snowflake's Cloud data platform.
+            - Data types
+            - Optimizer
+            - Continuous data protection
+            - Cloning
+            - Types of Caching
+            - User-defined Functions (UDFs)
+            - Web Interface (UI)
+            - Data Cloud/Data Sharing/Data Marketplace/Data Exchange
+            ##### 5.2 Outline Snowflake data sharing capabilities.
+            - Account types
+            - Data Marketplace and Exchange
+            - Access control options
+            - Shares
+            ##### 5.3 Explain how Snowflake is different compared to legacy warehouse solutions.
+            - Elastic Storage
+            - Elastic Compute
+            - Account Management
+            ##### 5.4 Outline the different editions that are available, and the functionality associated with each edition.
+            - Pricing
+            - Features
+            ##### 5.5 Identify Snowflake's Partner Ecosystem.
+            - Cloud Partners
+            - Connectors
+            ##### 5.6 Outline and define the purpose of Snowflake's three distinct layers.
+            - Storage Layer
+            - Compute Layer
+            - Cloud Services Layer
+            ##### 5.7 Outline Snowflake's catalog and objects.
+            - Accounts
+            - Database
+            - Schema
+            - Table Types
+            - View Types
+            - Data Types
+            - External Functions
+            - Stored Procedures
+            """)
+        
+        # Storage and Protection
+        with tab11:
+            st.markdown("""
+            ##### 6.1 Outline Snowflake Storage Concepts.
+            - Micropartitions
+            - Metadata Types
+            - Clustering
+            - Data Storage
+            - Stage Types
+            - File Formats
+            - Storage Monitoring
+            ##### 6.2 Outline Continuous Data Protection with Snowflake.
+            - Time Travel
+            - Fail Safe
+            - Data Encryption
+            - Cloning
+            - Replication
+            - Master keys
+            """)
+
+# Selector
+with tab3:
+    print("")
+
+# Randomizer
+with tab4:
     st.subheader("Randomizer")
     if st.button("Generate Study Topic"):
         link = random.choice(links)
@@ -28,7 +198,8 @@ with tab2:
     else:
         st.write("")
 
-with tab3:
+# Sample Questions
+with tab5:
     st.subheader("Sample Questions")
     q1 = st.radio("Which type of Data Integration tools leverage Snowflake's scalable compute for data transformation?",("Database Replication", "ELT", "ETL", "Streaming"))
     if q1 == "ELT":
--- pages/5_❄️_Data_Scientist.py
+++ pages/5_❄️_Data_Scientist.py
@@ -8,11 +8,16 @@ import pandas as pd
 import random
 from links.ds import links
 
+# Page Layout
+st.set_page_config(layout="wide")
+
 # App Code
 st.header("SnowPro Advanced: Data Scientist")
 
-tab1, tab2, tab3 = st.tabs(["Exam Breakdown", "Randomizer", "Sample Questions"])
+# Tabs
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Exam Breakdown", "Domains & Objectives", "Selector", "Randomizer", "Sample Questions"])
 
+# Exam Breakdown
 with tab1:
     st.subheader("Exam Breakdown")
     dsbd = {"Domain": ["Data Science Concepts", "Data Pipelining", "Data Preparation and Feature Engineering", "Model Development", "Model Deployment"],
@@ -20,7 +25,212 @@ with tab1:
     df = pd.DataFrame(data=dsbd)
     st.table(df)
 
+# Domains & Objectives
 with tab2:
+    st.subheader("Domains & Objectives")
+    with st.container():
+        tab6, tab7, tab8, tab9, tab10 = st.tabs(["Data Science Concepts", "Data Pipelining", "Data Preparation and Feature Engineering", "Model Development", "Model Deployment"])
+        st.markdown('''
+                    <style>
+                    [data-testid="stMarkdownContainer"] ul{
+                        padding-left:40px;
+                    }
+                    </style>
+                    ''', unsafe_allow_html=True)
+
+        # Data Science Concepts
+        with tab6:
+            st.markdown("""
+            ##### 1.1 Define machine learning concepts for data science workloads.
+            - Artificial Intelligence
+            - Machine Learning
+            ##### 1.2 Outline machine learning problem types.
+            - Supervised Learning
+              + Structured Data
+                + Linear Regression
+                + Binary Classification
+                + Multi-class Classification
+                + Time-Series Forecasting
+              + Unstructured Data
+                + Image Classification, segmentation, Language Modeling, QnA
+                + Typical DNN architectures: CNN, RNN, Transformer, BERT
+            - Unsupervised Learning
+              + Clustering
+            ##### 1.3 Summarize the machine learning lifecycle.
+            - Data Collection
+            - Data Visualization and Exploration
+            - Feature engineering
+            - Training models
+            - Model deployment
+            - Model monitoring and evaluation
+            ##### 1.4 Outline data governance for data science.
+            - Dynamic data masking
+            - Row level security
+            - Role Based Access Control (RBAC)
+            ##### 1.5 Outline statistical concepts for data science.
+            - Normal distribution
+            - Central limit theorem
+            - Z and T tests
+            - Bootstrapping
+            - Confidence intervals
+            ##### 1.6 Define model governance for data science.
+            - Model versioning
+            - Lineage
+            - Model explainability
+            """)
+
+        # Data Pipelining
+        with tab7:
+            st.markdown("""
+            ##### 2.1 Source and collect data into Snowflake from multiple sources.
+            - Data loading
+              + Batch loading
+              + Snowpipe
+              + External tables
+              + Materialized views
+              + Streams
+              + Tasks
+            - Connecting ETL tools (Connectors)
+            ##### 2.2 Enrich data by consuming data sharing sources.
+            - Data Marketplace
+            - Direct Sharing
+            - Shared database considerations
+            ##### 2.3 Create a development environment (e.g., sandbox) and maintain the environment.
+            - Cloning
+            - Levels or hierarchy
+              + Table or schema or database
+            - Automation to keep dataset updated
+            - Time Travel
+            ##### 2.4 Build a data science pipeline.
+            - Automation of data transformation
+            - Streams and tasks
+            - Functions
+            - Stored procedures
+            - Connect Snowflake to machine learning platforms (e.g., connectors, ML partners, etc.)
+            """)
+
+        # Data Preparation and Feature Engineering
+        with tab8:
+            st.markdown("""
+            ##### 3.1 Prepare and clean the data for analysis in Snowflake.
+            - Use Snowflake native functions, SQL, and Snowpark
+              + Aggregate
+              + Joins
+              + Common Table Expressions (CTEs)
+              + Identify critical data
+              + Remove duplicates
+              + Remove irrelevant fields
+              + Handle missing values
+              + Data type casting
+              + Sampling data
+              + Tasks to automate repeatable jobs
+              + Stored procedures
+            ##### 3.2 Perform feature engineering on Snowflake data.
+            - Preprocessing
+            - Data transformations
+              + Data Frames
+              + Snowpark (preview)
+            - Binarizing data
+              + Binning continuous data into intervals
+              + Label encoding
+              + One hot encoding
+            - Time Travel 
+            ##### 3.3 Perform exploratory data analysis in Snowflake.
+            - Snowsight and SQL
+              + Identify initial patterns
+              + Connect external machine learning platforms and/or notebooks (e.g., Jupyter)
+            - Use Snowflake native statistical functions to analyze and calculate descriptive data statistics.
+              + Window Functions
+              + MIN/MAX/AVG/STDEV
+              + VARIANCE
+              + TOPn
+              + Approximation/High Performing function
+            - Linear Regression
+              + Find the slope and intercept
+              + Verify the dependencies on dependent and independent variables
+            ##### 3.4 Visualize and interpret the data to present a business case.
+            - Statistical summaries
+              + Snowsight
+              + Snowflake SQL
+              + Functions
+            - Charts and graphs
+              + Identify data outliers
+            """)
+
+        # Model Development
+        with tab9:
+            st.markdown("""
+            ##### 4.1 Connect data science tools directly to data in Snowflake.
+            - Connectors
+              + Python connector with panda support
+              + Spark connector
+              + R connector
+            - Snowflake Best Practices
+              + One platform, one copy of data, many workloads
+              + Enrich datasets using the data marketplace
+              + Stream and Tasks
+              + External tables
+              + External functions to trigger training
+              + Zero-copy cloning for training snapshots
+              + Materialized views for training and prediction
+              + Snowflake SQL for aggregation and sampling
+            ##### 4.2 Train a data science model.
+            - Hyperparameter tuning
+            - Optimization metric selection (e.g., log loss, AUC, RMSE)
+            - Partitioning
+              + Cross validation
+              + Train, validation, holdout
+            - Down/Up-sampling
+            ##### 4.3 Validate a data science model.
+            - ROC curve/confusion matrix
+              + Calculate the expected payout of the model
+            - Regression problems
+            - Residuals plot
+              + Interpret graphics with context
+            - Model metrics
+
+            ##### 4.4 Interpret a model.
+            - Feature impact
+            - Partial depedence plots
+            - Confidence intervals
+            """)
+
+        # Model Deployment
+        with tab10:
+            st.markdown("""
+            ##### 5.1 Move a data science model into production.
+            - Deploy an external hosted model
+              + External functions
+              + Pre-generated models from third party vendors
+            - Deploy a model in Snowflake
+              + Java User Defined Functions (UDFs)
+              + Pre-generated models from third party vendors
+            ##### 5.2 Score the effectiveness of a model and retrain if necessary.
+            - Metrics for model evaluation
+              + Data drift /Model decay
+                + Data distribution comparisons
+                  + Does the data making predictions look similar to the training data?
+                  + Do the same data points give the same predictions once a model is deployed?
+            - External functions
+            - User defined functions (UDFs)
+            - Storing predictions
+              + Stage commands
+            - Use Snowsight to do distribution comparison
+            ##### 5.3 Outline model lifecycle and validation tools.
+            - Streams and Tasks
+            - Metadata tagging
+            - Partner model versioning
+              + Source control
+              + Git workflow
+            - Automation of model retraining
+            """)
+
+# Selector
+with tab3: 
+    print("")
+
+# Randomizer
+with tab4:
     st.subheader("Randomizer")
     if st.button("Generate Study Topic"):
         link = random.choice(links)
@@ -28,7 +238,8 @@ with tab2:
     else:
         st.write("")
 
-with tab3:
+# Sample Questions
+with tab5:
     st.subheader("Sample Questions")
     q1l1 = st.write("Where do the majority of Data Scientists spend the MOST time and effort during the Data Science workflow?")
     q1l2 = st.write("A. Building ETL pipelines")
