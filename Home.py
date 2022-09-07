ae88ac15dbd9d17b914794c577e571f8bf963b0d
Emily Nguyen <101374243+thedatarubicon@users.noreply.github.com>
2022-09-07 17:46:23 -0500

    Update Homepage
    Updated homepage by consolidating all of the text into one st.markdown.

--- Home.py
+++ Home.py
@@ -1,20 +1,35 @@
 # Import package dependencies
 import streamlit as st
 
+# Page Layout
+st.set_page_config(layout="wide")
+st.markdown('''
+                    <style>
+                    [data-testid="stMarkdownContainer"] ul{
+                        padding-left:40px;
+                    }
+                    </style>
+                    ''', unsafe_allow_html=True)
+
 # App Code
 st.header("SnowPro Study Guide Randomizer")
 
-st.subheader("Table of Contents")
-st.markdown("I. SnowPro Core")
-st.markdown("II. SnowPro Advanced: Architect")
-st.markdown("III. SnowPro Advanced: Administrator")
-st.markdown("IV. SnowPro Advanced: Data Engineer")
-st.markdown("V. SnowPro Advanced: Data Scientist")
-st.markdown("")
-st.subheader("Page Overview")
-st.markdown("A. Exam Breakdown")
-st.markdown("B. Randomizer")
-st.markdown("C. Sample Questions")
-st.markdown("")
-st.subheader("References")
-st.markdown("[Snowflake Certifications](https://www.snowflake.com/certifications/)")
+# Page Content
+st.markdown("""
+#### Table of Contents
+- SnowPro Core
+- SnowPro Advanced: Architect
+- SnowPro Advanced: Administrator
+- SnowPro Advanced: Data Engineer
+- SnowPro Advanced: Data Scientist
+- Feedback
+#### Page Overview
+Each page contains the following tabs:
+- Exam Breakdown
+- Domains & Objectives
+- Selector
+- Randomizer
+- Sample Questions
+#### References
+[Snowflake Certifications](https://www.snowflake.com/certifications/)
+""")
