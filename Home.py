7bf4c1f24e1748fd5cb11d7f664e9696f0e58aed
Emily Nguyen <101374243+thedatarubicon@users.noreply.github.com>
2022-09-08 18:18:40 -0500

    Restructure Home page
    Restructured Home page to a multi-tab structure with the following tabs: overview and release notes.

--- Home.py
+++ Home.py
@@ -14,22 +14,53 @@ st.markdown('''
 # App Code
 st.header("SnowPro SnowStudy")
 
+tab1, tab2 = st.tabs(["Overview", "Release Notes"])
+
 # Page Content
-st.markdown("""
-#### Table of Contents
-- SnowPro Core (Last Updated: August 9, 2022)
-- SnowPro Advanced: Architect (Last Updated: May 3, 2022)
-- SnowPro Advanced: Administrator (Last Updated: March 1, 2022)
-- SnowPro Advanced: Data Engineer (Last Updated: June 23, 2022)
-- SnowPro Advanced: Data Scientist (Last Updated: May 17, 2022)
-- Feedback
-#### Page Overview
-Each page contains the following tabs:
-- Exam Breakdown
-- Domains & Objectives
-- Study Resources
-- Randomizer
-- Sample Questions
-#### References
-[Snowflake Certifications](https://www.snowflake.com/certifications/)
-""")
+
+# Overview
+with tab1:
+    st.markdown("""
+    #### SnowPro Study Guides
+    - Core 
+    - Advanced: Architect
+    - Advanced: Administrator
+    - Advanced: Data Engineer
+    - Advanced: Data Scientist
+    - Feedback
+    #### Page Structure
+    Each page contains the following tabs:
+    - Exam Breakdown
+    - Domains & Objectives
+    - Study Resources
+    - Randomizer
+    - Sample Questions
+    #### References
+    - [Snowflake Certifications](https://www.snowflake.com/certifications/)
+    """)
+
+# Release Notes
+with tab2:
+    st.markdown("""
+    ##### 9/8/22:
+    - Update Core page's structure and content
+    - Update Core page's list object references from links folder
+    - Add "Last Updated" date to the Home page
+    - Update the Home header
+    - Update the README title
+    - Add "Last Updated" date to each exam-specific page
+    - Restructure/Rework Sample Questions tab
+    - Restructure/Rework Randomizer tab to display URL along with iframe component
+    - Update Home page structure to have the following tabs: Overview, Release Notes
+    ##### 9/7/22:
+    - Add Domains & Objectives tab to each exam-specific page
+    - Add Feedback page to app
+    - Add all exam-specific links to the Study Resources tab
+    - Make all links into clickable links in Study Resources tab
+    ##### 8/31/22: 
+    - SnowPro SnowStudy's exam-specific pages were built out
+    - Randomizer tab's iframe component's width and height was adjusted
+    - Update README
+    ##### 8/30/22: 
+    - SnowPro SnowStudy app was created
+    """)
